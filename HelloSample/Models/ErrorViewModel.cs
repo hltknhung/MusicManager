01ea7ae7185a2456a93977b52902dd3801f1ebb8
Vo Van Hai <vovanhai@monetplus.cz>
2020-10-07 12:51:46 +0200

    Add project files.

---
+++ HelloSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
